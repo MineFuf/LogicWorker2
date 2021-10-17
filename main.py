e8ac21d815b48330c40b1cde610031bc699b699f
MineFuf <honys.mi.2020@skola.ssps.cz>
2021-10-17 11:42:08 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+import random
+from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtCore import Slot
+
+
+class LogicGeneratorWidget(QtWidgets.QWidget):
+    def __init__(self, t):
+        super().__init__()
+
+        layout = QtWidgets.QVBoxLayout()
+        self.setLayout(layout)
+
+        layout.addWidget(QtWidgets.QLabel(t))
+
+
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QtWidgets.QGridLayout()
+        self.setLayout(layout)
+        self.setStyleSheet(open('resources/main_style.css', 'r').read())
+
+        button = QtWidgets.QPushButton("Man press me")
+        button.clicked.connect(lambda: print('hello'))
+
+        tab_layout = QtWidgets.QTabWidget()
+        tab_layout.addTab(LogicGeneratorWidget('kys'), "Table To Logic")
+        tab_layout.addTab(button, 'Button')
+        layout.addWidget(tab_layout)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    size = (600, 400)
+
+    widget = Window()
+    widget.resize(*size)
+    widget.setFixedSize(*size)
+    widget.show()
+
+    sys.exit(app.exec())
